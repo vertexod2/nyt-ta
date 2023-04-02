22aef3fa45a1ec47fc8f57b083bce4722fccb3a9
Taras Bezditnyi <taras@vertexod.com>
2023-04-02 02:21:26 +0200

    add jsonresponse middleware for all api calls

---
+++ app/Http/Middleware/ApiJsonResponse.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiJsonResponse
+{
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $request->headers->set('Accept', 'application/json');
+
+        return $next($request);
+    }
+}
